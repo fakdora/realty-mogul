7860cb011460cee3cf5e0cf39cf3180192bb3909
David <davidleesd@gmail.com>
2018-07-08 18:21:57 -0700

    remove warnings and errs

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 
-function Panel(panelData, userClickHandler, visibility) {
+function Panel({panelData, userClickHandler, visibility}) {
     return (
       <div
         style={{ backgroundColor: visibility ? "#FFFFFF" : '#EEEEEE' }}
